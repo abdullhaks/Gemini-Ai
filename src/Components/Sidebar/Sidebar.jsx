649d0c86eb40c80c7550016906660820b66cb1c0
abdullhaks <aks4u786@gmail.com>
2025-01-27 09:38:52 +0530

    first commit - folder structered , side bar set up start

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      
+      <div className="top">
+
+      </div>
+
+      <div className="footer">
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
