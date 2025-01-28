89bfba82a374892208ad4b0542220237176a7023
abdullhaks <aks4u786@gmail.com>
2025-01-28 17:30:20 +0530

    request and responses done, history management done , new chat done

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,73 +1,104 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../../assets/assets';
 import './Main.css'
+import { Context } from '../../Context/context';
 
 function Main() {
 
 
+const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context);
 
-  
   return (
-    <div className='main'>
-        
-        <div className="nav">
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt="" />
-        </div>
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
 
-        <div className="main-container">
-
-          <div className="greet">
-            <p><span>Hello, Dev.</span></p>
-            <p>How can I help you today?</p>
-          </div>
-
-          <div className="cards">
-
-            <div className="card">
-              <p>Suggest some beautifull places in kerala for a oneday trip.</p>
-              <img src={assets.compass_icon} alt="" />
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
+              <p>How can I help you today?</p>
             </div>
 
-            <div className="card">
-              <p>give me a beautifull topic to a presentaion with some intresting points.</p>
-              <img src={assets.bulb_icon} alt="" />
-            </div>
+            <div className="cards">
+              <div className="card">
+                <p>
+                  Suggest some beautifull places in kerala for a oneday trip.
+                </p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
 
-            <div className="card">
-              <p>I want to learn react . give me a detail road map.</p>
-              <img src={assets.message_icon} alt="" />
-            </div>
+              <div className="card">
+                <p>
+                  give me a beautifull topic to a presentaion with some
+                  intresting points.
+                </p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
 
-            <div className="card">
-              <p>what is redux.give basic and adavace things about it.</p>
-              <img src={assets.code_icon} alt="" />
-            </div>
+              <div className="card">
+                <p>I want to learn react . give me a detail road map.</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
 
-          </div>
-
-          <div className="main-bottom">
-
-            <div className="search-box">
-
-              <input type="text" placeholder='Enter a prompt here' />
-              <div>
-                <img src={assets.gallery_icon} alt="" />
-                <img src={assets.mic_icon} alt="" />
-                <img src={assets.send_icon} alt="" />
+              <div className="card">
+                <p>what is redux.give basic and adavace things about it.</p>
+                <img src={assets.code_icon} alt="" />
               </div>
             </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
 
-            <p className="bottom-info">
-              Gemimi may display inaccurate info, including about people, so double check its reponse. Your privacy and Gemini App
-            </p>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
 
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+              
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              type="text"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              placeholder="Enter a prompt here"
+            />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} onClick={() => onSent()} alt="" />
+            </div>
           </div>
 
+          <p className="bottom-info">
+            Gemimi may display inaccurate info, including about people, so
+            double check its reponse. Your privacy and Gemini App
+          </p>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
 export default Main
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,58 +1,75 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import  './Sidebar.css'
 import {assets} from '../../assets/assets'
+import { Context } from '../../Context/context';
 
 function Sidebar() {
 
 const [extended,setExtended] = useState(false);
 function handleClick(){
   setExtended((extended)=>extended?false:true)
+};
+
+const {onSent,prevPrompts,setRecentPrompt,newChat} = useContext(Context);
+const loadPrompt = (promt)=>{
+  setRecentPrompt(promt)
+  onSent(promt)
 }
 
   return (
-    <div className='sidebar'>
-      
+    <div className="sidebar">
       <div className="top">
-        <img className='menu' onClick={handleClick} src={assets.menu_icon} alt="Menu" />
-        <div className="new_chat">
+        <img
+          className="menu"
+          onClick={handleClick}
+          src={assets.menu_icon}
+          alt="Menu"
+        />
+        <div className="new_chat" onClick={()=>newChat()}>
           <img src={assets.plus_icon} alt="." />
-          {extended &&  <p>New Chat</p>}
-        
+          {extended && <p>New Chat</p>}
         </div>
 
-        {extended && 
+        {extended && (
           <div className="recent">
-
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="." />
-              <p>What is React.....</p>
-            </div>
-          </div>
 
-        }
+            {prevPrompts.map((item, indx) => {
+              return (
+                <div
+                  className="recent-entry"
+                  key={indx}
+                  onClick={() => {
+                    loadPrompt(item);
+                  }}
+                >
+                  <img src={assets.message_icon} alt="." />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
 
       <div className="bottom">
-
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="?" />
-          {extended && <p>Help</p> }
+          {extended && <p>Help</p>}
         </div>
 
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="?" />
-          {extended && <p>Activity</p> }
+          {extended && <p>Activity</p>}
         </div>
 
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="?" />
-          {extended && <p>Settings</p> }
+          {extended && <p>Settings</p>}
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
 export default Sidebar
